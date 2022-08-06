4aaf79151dc19e3581ebc6b2bac51ed631748cec
ESPboy <ESPboy.edu@gmail.com>
2022-08-06 22:15:23 +0300

    Add game Pixonia

---
+++ GAMES/Pixonia_Pokitto/Pixonia.h
@@ -0,0 +1,67 @@
+#ifndef POK_POKULOUS_H
+#define POK_POKULOUS_H
+
+#include <stdint.h>
+#include "Arduino.h"
+
+extern PROGMEM const uint16_t pokulous_pal[];
+extern PROGMEM const uint16_t theresgold_pal[];
+extern PROGMEM const uint16_t gameover_pal[];
+
+extern PROGMEM const uint8_t anim_dude[];
+extern PROGMEM const uint8_t anim_sheep[];
+extern PROGMEM const uint8_t anim_wolf_sw[];
+extern PROGMEM const uint8_t anim_blue_flag1[];
+extern PROGMEM const uint8_t anim_blue_flag2[];
+extern PROGMEM const uint8_t splatter[];
+extern PROGMEM const uint8_t theresgold[];
+extern PROGMEM const uint8_t gameover[];
+extern PROGMEM const uint8_t gold[];
+extern PROGMEM const uint8_t gold2[];
+
+extern PROGMEM const uint8_t pixelman_se_1[];
+extern PROGMEM const uint8_t pixelman_se_2[];
+extern PROGMEM const uint8_t pixelman_nw_1[];
+extern PROGMEM const uint8_t pixelman_nw_2[];
+
+extern PROGMEM const uint8_t werewolf_se_1[];
+extern PROGMEM const uint8_t werewolf_se_2[];
+extern PROGMEM const uint8_t werewolf_nw_1[];
+extern PROGMEM const uint8_t werewolf_nw_2[];
+
+
+extern PROGMEM const uint8_t ter_beach_se[];
+extern PROGMEM const uint8_t ter_beach_sw[];
+
+extern PROGMEM const uint8_t ter_bump_e[];
+extern PROGMEM const uint8_t ter_bump_n[];
+extern PROGMEM const uint8_t ter_bump_w[];
+extern PROGMEM const uint8_t ter_bump_s[];
+
+extern PROGMEM const uint8_t ter_ditch_e[];
+extern PROGMEM const uint8_t ter_ditch_n[];
+extern PROGMEM const uint8_t ter_ditch_w[];
+extern PROGMEM const uint8_t ter_ditch_s[];
+
+extern PROGMEM const uint8_t ter_bump_ne[];
+extern PROGMEM const uint8_t ter_bump_nw[];
+extern PROGMEM const uint8_t ter_bump_sw[];
+extern PROGMEM const uint8_t ter_bump_se[];
+
+extern PROGMEM const uint8_t ter_earth[];
+extern PROGMEM const uint8_t ter_grain[];
+extern PROGMEM const uint8_t ter_gold[];
+extern PROGMEM const uint8_t ter_plain[];
+extern PROGMEM const uint8_t ter_hut[];
+extern PROGMEM const uint8_t ter_water1[];
+extern PROGMEM const uint8_t ter_water2[];
+extern PROGMEM const uint8_t ter_tree[];
+extern PROGMEM const uint8_t ter_stone[];
+
+extern PROGMEM const uint8_t dirt_pillar[];
+extern PROGMEM const uint8_t pixonia[];
+extern PROGMEM const uint16_t night_palette[];
+
+
+
+#endif // POK_POKULOUS_H
---
+++ GAMES/Pixonia_Pokitto/splatter.cpp
@@ -0,0 +1,19 @@
+
+
+/*
+ * BMP image as 4bpp (16 colour index) data
+ */
+
+#include <stdint.h>
+#include "Pixonia.h"
+
+PROGMEM const uint8_t splatter[] = {
+8,8,
+255,255,255,255,255,239,255,255,
+255,255,255,255,255,255,239,255,
+255,255,255,255,255,255,255,254,
+255,239,255,255,255,255,255,255,
+
+};
+
+
