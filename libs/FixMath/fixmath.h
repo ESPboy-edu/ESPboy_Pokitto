dd1420e8c052b577e64e06c755269e6fa6b9ee6d
ESPboy <espboy.edu@gmail.com>
2024-05-24 07:03:35 +0300

    add FixMath

---
+++ libs/FixMath/fixmath.h
@@ -0,0 +1,25 @@
+#ifndef __libfixmath_fixmath_h__
+#define __libfixmath_fixmath_h__
+
+
+
+/*!
+	\file fixmath.h
+	\brief Functions to perform fast accurate fixed-point math operations.
+*/
+
+#include "uint32.h"
+#include "int64.h"
+#include "fract32.h"
+#include "fix16.h"
+
+#include "fix16_trig_sin_lut.h"
+#include "fix16_exp.cpp"
+#include "fix16_sqrt.cpp"
+#include "fix16_trig.cpp"
+#include "fix16.cpp"
+#include "fix16.hpp"
+#include "fract32.cpp"
+#include "uint32.cpp"
+
+#endif
